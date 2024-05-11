3521d3a1a806f1bf86224eee634245e6b684d1a4
wyt_cpri <83196369+junwooYeom@users.noreply.github.com>
2024-05-11 17:04:42 +0900

    💄커스텀 폰트 추가

---
+++ design/src/main/java/com/zerosome/design/ui/theme/Type.kt
@@ -0,0 +1,107 @@
+@file:Suppress("UNUSED")
+
+package com.zerosome.design.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.zerosome.design.R
+import com.zerosome.design.ui.extension.toSp
+
+private val PretendardFontFamily = FontFamily(
+    listOf(
+        Font(R.font.pretendard_black, weight = FontWeight.Black, style = FontStyle.Normal),
+        Font(R.font.pretendard_extra_bold, weight = FontWeight.ExtraBold, style = FontStyle.Normal),
+        Font(R.font.pretendard_bold, weight = FontWeight.Bold, style = FontStyle.Normal),
+        Font(R.font.pretendard_semi_bold, weight = FontWeight.SemiBold, style = FontStyle.Normal),
+        Font(R.font.pretendard_medium, weight = FontWeight.Medium, style = FontStyle.Normal),
+        Font(R.font.pretendard_regular, weight = FontWeight.Normal, style = FontStyle.Normal),
+        Font(R.font.pretendard_light, weight = FontWeight.Light, style = FontStyle.Normal),
+        Font(
+            R.font.pretendard_extra_light,
+            weight = FontWeight.ExtraLight,
+            style = FontStyle.Normal
+        ),
+        Font(R.font.pretendard_thin, weight = FontWeight.Thin, style = FontStyle.Normal)
+    )
+)
+
+val H1
+    @Composable
+    get() = TextStyle(
+        fontFamily = PretendardFontFamily,
+        fontSize = 20.dp.toSp(),
+        fontWeight = FontWeight.Bold,
+        lineHeight = (20.dp.times(other = 1.35f).toSp()),
+        letterSpacing = (-0.01).sp
+    )
+
+val H2
+    @Composable
+    get() = TextStyle(
+        fontFamily = PretendardFontFamily,
+        fontSize = 18.dp.toSp(),
+        fontWeight = FontWeight.Bold,
+        lineHeight = (18.dp.times(other = 1.35f).toSp()),
+        letterSpacing = (-0.01).sp
+    )
+
+val SubTitle1
+    @Composable
+    get() = TextStyle(
+        fontFamily = PretendardFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 15.dp.toSp(),
+        lineHeight = (15.dp.times(1.4f).toSp()),
+    )
+
+val SubTitle2
+    @Composable
+    get() = TextStyle(
+        fontFamily = PretendardFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.dp.toSp(),
+        lineHeight = (14.dp.times(1.4f).toSp()),
+    )
+
+val Body1
+    @Composable
+    get() = TextStyle(
+        fontFamily = PretendardFontFamily,
+        fontSize = 15.dp.toSp(),
+        fontWeight = FontWeight.Medium,
+        lineHeight = (15.dp.times(1.4f).toSp()),
+    )
+
+
+val Body2
+    @Composable
+    get() = TextStyle(
+        fontFamily = PretendardFontFamily,
+        fontSize = 14.dp.toSp(),
+        fontWeight = FontWeight.Medium,
+        lineHeight = (14.dp.times(1.4f).toSp()),
+    )
+
+val Label
+    @Composable
+    get() = TextStyle(
+        fontFamily = PretendardFontFamily,
+        fontSize = 13.dp.toSp(),
+        fontWeight = FontWeight.Medium,
+        lineHeight = (13.dp.times(1.4f).toSp()),
+    )
+
+val Caption
+    @Composable
+    get() = TextStyle(
+        fontFamily = PretendardFontFamily,
+        fontSize = 13.dp.toSp(),
+        fontWeight = FontWeight.Medium,
+        lineHeight = (13.dp.times(1.4f).toSp()),
+    )
