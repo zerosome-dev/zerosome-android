5bfb7a3cddfa1135fb4c173895748ae4caa04035
wyt_cpri <83196369+junwooYeom@users.noreply.github.com>
2024-05-29 16:51:40 +0900

    💄 Modify Onboarding Navigation = Popup to Onboarding Route.

--- app/src/main/java/com/zerosome/android/MainActivity.kt
+++ app/src/main/java/com/zerosome/android/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
@@ -18,7 +20,7 @@ class MainActivity : ComponentActivity() {
             ZerosomeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize().statusBarsPadding().navigationBarsPadding(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     ZSNavHost()
--- feat/onboarding/src/main/java/com/zerosome/onboarding/NicknameScreen.kt
+++ feat/onboarding/src/main/java/com/zerosome/onboarding/NicknameScreen.kt
@@ -1,16 +1,37 @@
 package com.zerosome.onboarding
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.zerosome.design.ui.theme.Body1
+import com.zerosome.design.ui.theme.Body2
+import com.zerosome.design.ui.theme.H2
 import com.zerosome.design.ui.theme.Label2
+import com.zerosome.design.ui.theme.ZSColor
 
 @Composable
 internal fun NicknameScreen(onClickNext: () -> Unit) {
-    Button(onClick = onClickNext) {
-        Text("닉네임 설정 화면", modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center, style = Label2)
+    Column(modifier = Modifier.fillMaxSize()) {
+        Spacer(modifier = Modifier.height(30.dp))
+        Text(text = "닉네임을 설정해주세요", style = H2)
+        Text(text = "최소 2자 ~ 12자 이내의 닉네임을 입력해주세요", style = Body2, color = ZSColor.Neutral400)
+        Spacer(modifier = Modifier.weight(1f))
+        Button(onClick = onClickNext) {
+            Text(
+                "닉네임 설정 화면",
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                style = Label2
+            )
+        }
     }
+
 }
