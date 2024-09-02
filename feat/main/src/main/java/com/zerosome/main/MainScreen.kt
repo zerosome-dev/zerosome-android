bb886d191583a6204682d368095705d422785055
wyt_cpri <83196369+junwooYeom@users.noreply.github.com>
2024-09-02 22:03:12 +0900

    Rebase 설정.

--- feat/main/src/main/java/com/zerosome/main/MainScreen.kt
+++ feat/main/src/main/java/com/zerosome/main/MainScreen.kt
@@ -17,11 +17,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat.startActivity
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -67,7 +65,7 @@ fun MainScreen(onMoveToLogin: () -> Unit) {
                     }
                 )
             }
-            profileNavigation(navController) {
+            profileNavigation(navController, onMoveToLogin = onMoveToLogin) {
                 val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(it))
                 context.startActivity(browserIntent)
             }
