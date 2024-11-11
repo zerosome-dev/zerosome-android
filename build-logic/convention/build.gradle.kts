6849a97daf39fa3d52467e6ec43d53ae383f602d
wyt_cpri <83196369+junwooYeom@users.noreply.github.com>
2024-11-11 20:48:26 +0900

    🔧 Add Configuration functions.

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -5,4 +5,9 @@ plugins {
 group = "com.zerosom.android.buildlogic"
 
 dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.android.tools.common)
+    compileOnly(libs.compose.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
 }
